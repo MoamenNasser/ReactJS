69a1fdb41502df50979660e9afb247aa18655b87
MoamenNasser <65139131+MoamenNasser@users.noreply.github.com>
2025-10-08 01:08:43 +0300

    Remove unused files and reorganize project structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactJS
-Reviewing my React Knowledge 
--- src/app/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { } from "react-dom";
-
-
-
-const root = createRoot(document.getElementById("root"));
-root.render(<h1>Hello, World!</h1>);
