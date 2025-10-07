01bf41c3c58b294ab78fa86f89dfb8bc677c2b00
MoamenNasser <65139131+MoamenNasser@users.noreply.github.com>
2025-10-08 01:16:39 +0300

    Review

--- MODAL_Window_Game/script.js
+++ MODAL_Window_Game/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// use the querySelectorAll to select all buttons with the class 'show-modal' (if not used, only the first button will be selected)
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+
+console.log(btnsOpenModal);
+
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+
